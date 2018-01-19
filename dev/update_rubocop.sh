e119967c2c634aa5d36b2d0ccc7840ccc53a0e44
Brian Dwyer <bdwyer@IEEE.org>
2018-01-19 16:30:48 -0500

    Cut Rubocop config to Cookstyle in a cleaner fashion

--- dev/update_rubocop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update Rubocop
-
-mkdir .cookstyle
-
-for style in cookstyle cookstyle_base default disabled disable_all enabled upstream
-do
-  curl -sL https://raw.githubusercontent.com/chef/cookstyle/master/config/${style}.yml -o .cookstyle/${style}.yml
-done
